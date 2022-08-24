cfe32508f7d893ff0588edc6ffe51de45546e1d1
Dipak shinde <deepakshinde3346@gmail.com>
2022-08-24 16:01:08 +0530

    dashborad navbar

--- FROENT/src/Components/Navbar.jsx
+++ FROENT/src/Components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../Pages/Dashbord/Dashboard.jsx";
+import WithSubnavigation from "./Navbar/Navbar.jsx";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<WithSubnavigation />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
