473868fddb020de32fff60719f97967b8e0ec70e
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-08-24 09:39:17 +0530

    done with form design

--- FROENT/src/App.js
+++ FROENT/src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
-import Navbar from './Components/Navbar';
-
+// import Navbar from './Components/Navbar'
+import {Login} from './Pages/Login';
+import {Routes,Route} from "react-router-dom"
+import {Signup} from "./Pages/Signup"
 function App() {
   return (
     <div >
-      <Navbar/>
+      <Routes>
+      <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+  
+
     </div>
   );
 }
