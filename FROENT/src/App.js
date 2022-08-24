4dd8035681855884f5fb05ee984c5cea5fd26df6
jitesh-95 <1995sirohis@gmail.com>
2022-08-24 23:36:09 +0530

    blog section data collection done

--- FROENT/src/App.js
+++ FROENT/src/App.js
@@ -1,28 +1,23 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import List from './Components/List';
-import Navbar from './Components/Navbar';
-import Projects from './Pages/Projects';
-import {Login} from './Pages/Login';
-import {Signup} from "./Pages/Signup"
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import List from "./Components/List";
+import Navbar from "./Components/Navbar";
+import Projects from "./Pages/Projects";
+import { Login } from "./Pages/Login";
+import { Signup } from "./Pages/Signup";
+import Blog from "./Pages/blogs/Blog";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
+    <div>
+      <Navbar />
       <Routes>
-        <Route path='/project' element={<Projects />} />
-              <Route path="/login" element={<Login/>}></Route>
-                <Route path="/signup" element={<Signup/>}></Route>
-        <Route path='/project/List' element={<List />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/project/List" element={<List />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
-
-
-
-
-
-
     </div>
   );
 }
