4e62dfd3340d08e345d903ab97155a7b83bc8fe3
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-08-24 09:49:06 +0530

    ,

--- FROENT/src/index.js
+++ FROENT/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
