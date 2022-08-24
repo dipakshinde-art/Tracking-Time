b5a1fb1f4357a38541ced9580cc33531855b894f
GaganChandan2003 <101565932+GaganChandan2003@users.noreply.github.com>
2022-08-24 20:12:45 +0530

    40% done

--- FROENT/src/App.js
+++ FROENT/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import Landingpage from './Pages/Landingpage';
 
 function App() {
   return (
     <div >
       <Navbar/>
+      <Landingpage/>
     </div>
   );
 }
